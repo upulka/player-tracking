60a5258ee76dc97dd38a5c5c4c9966b7f551b22f
upulka <upulka.saku@gmail.com>
2025-11-10 22:30:30 +0530

    Commit Sample Video Information

--- download_videos.py
+++ download_videos.py
@@ -11,6 +11,14 @@ def download_sample_videos():
         # Example format:
         # "https://www.youtube.com/shorts/ABC123",
         # "https://www.youtube.com/watch?v=XYZ789",
+        "https://drive.google.com/file/d/19sLpwIDDZD7M9RuyTi3X2bjjNe4ZMVM5/view?usp=drive_link",
+        "https://drive.google.com/file/d/1QWbH0w5kNudsQCFaQfqESomlWA92HUb1/view?usp=drive_link",
+        "https://drive.google.com/file/d/1QodadGq-0BGSJKtGAX95JPmoebeOJF_u/view?usp=drive_link",
+        "https://drive.google.com/file/d/1BfhrfemKyLGTUVAe2B2SdegRJaAoV9qF/view?usp=drive_link",
+        "https://drive.google.com/file/d/1zJShupiWovFGGLBsJX8g2FydT5kPijlz/view?usp=drive_link",
+        "https://drive.google.com/file/d/1psQvfJd0Ft8qN08wLi5vt6mXH1twf0sr/view?usp=drive_link",
+        "https://drive.google.com/file/d/1jfdL5yiFj7SRr6MPbVqdhX93pwiEnQu3/view?usp=drive_link",
+        "https://drive.google.com/file/d/15TtwkLyAnxV0eyZYFXuzj-ouj3LjbtSD/view?usp=drive_link"
     ]
     
     if not sports_videos:
